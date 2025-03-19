1a25d8f69c40b4ca401ab4cce1893cc4a88ed38a
phouth-alt <phanphouth68@gmail.com>
2025-03-19 13:48:01 +0700

    add void man

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Profile Screen',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ProfileScreen(),
+    );
+  }
+}
+
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
