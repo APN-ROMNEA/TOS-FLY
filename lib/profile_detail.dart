e44f1d66a8060670914bf59197af2e6909a5e826
phouth-alt <phanphouth68@gmail.com>
2025-03-21 14:42:54 +0700

    don't khow

--- lib/profile_detail.dart
+++ lib/profile_detail.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ProfileDetailScreen extends StatelessWidget {
+  const ProfileDetailScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
